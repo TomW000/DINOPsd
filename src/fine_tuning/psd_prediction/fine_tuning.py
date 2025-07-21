114fad552107c5428874784b1e8c27c2c35a2410
TomW <tomw7@orange.fr>
2025-07-21 13:17:54 +0100

    psd

--- src/fine_tuning/psd_prediction/fine_tuning.py
+++ src/fine_tuning/psd_prediction/fine_tuning.py
@@ -5,13 +5,12 @@ from tqdm import tqdm
 import os 
 import matplotlib.pyplot as plt
 import umap
+from sklearn.metrics import f1_score
 
-from src.setup import model, device, model_weights_path
+from src.setup import model, device, model_weights_path, resize_size, feat_dim
 
 from src.fine_tuning.adaptor.AdaptFormer import augmented_model
-
-from .training import train_loader, test_loader
-
+from .sliding_window_dataset import get_dataset
 
 def training_block(model,
                    index, 
@@ -20,8 +19,6 @@ def training_block(model,
                    test_set, 
                    return_statistics, 
                    use_umap):
-
-    loss_fn = nn.BCELoss()
 
     if index == 1:
 
@@ -63,66 +60,72 @@ def training_block(model,
 
     loss_list = []
     prediction_list = []
-    test_accuracies = []
+    test_loss_list = []
+
+    patch_size = model[0].patch_size
+    def loss_fn(y_true, y_pred): f1_score(y_true, y_pred)
 
     for epoch in tqdm(range(nb_epochs), desc='--> Epochs'):
         model.train()
         epoch_loss_list = []
 
-        for embeddings, ground_truths in tqdm(train_set, desc='-> Training', leave=False):
-            embeddings = embeddings.to(device)
-            ground_truths = ground_truths.to(device).float().unsqueeze(1)  # Make sure targets are float for BCELoss
+        for patches, ground_truth in tqdm(train_set, desc='-> Training', leave=False):
+            ground_truth = ground_truth.to(device)
+            prediction = torch.zeros(ground_truth.shape)
+            H_patch, W_patch = ground_truth.shape
 
             optimizer.zero_grad()
-            outputs = model(embeddings).float() # Ensure float32
-
-            loss = loss_fn(outputs, ground_truths)
+            for n, patch in enumerate(patches):
+                patch = patch.to(device)
+                nb_patches_y, nb_patches_x = patch.shape[1:]
+                embeddings = model[0].forward_features(patch)["x_norm_patchtokens"].reshape(nb_patches_y, nb_patches_x, feat_dim)
+                central_embedding = embeddings[nb_patches_y//2, nb_patches_x//2]
+                central_embedding = central_embedding.unsqueeze(0)
+                output = model[1](central_embedding)
+                y_coord = n // H_patch
+                x_coord = n % W_patch
+                prediction[y_coord: y_coord + patch_size, x_coord: x_coord + patch_size] = torch.zeros(patch_size, patch_size) if round(output) == 0 else torch.ones(patch_size, patch_size)
+                
+            loss = loss_fn(ground_truth, prediction)
             loss.backward()
             optimizer.step()
 
-            epoch_loss_list.append(loss.item())
+            epoch_loss_list.append(loss)
 
         loss_list.append(np.mean(epoch_loss_list))
 
         # Evaluation phase
         model.eval()
         with torch.no_grad():
-            score = 0
-            total = 0
-            
+
             #UMAP
             if use_umap and epoch == 0:
-                UMAP_LIST, UMAP_LABELS = [], []
+                UMAP_LIST, UMAP_PREDICTED_LABELS = [], []
                 UMAP_EMBEDDINGS = []
-                
-            for embeddings, ground_truths in tqdm(test_set, desc='-> Testing', leave=False):
-                
-                if use_umap and epoch == 0:    
+
+            for patches, ground_truth in tqdm(test_set, desc='-> Testing', leave=False):
+                ground_truth = ground_truth.to(device)
+                prediction = torch.zeros(ground_truth.shape)
+                H_patch, W_patch = ground_truth.shape
+
+                optimizer.zero_grad()
+                for n, patch in enumerate(patches):
+                    patch = patch.to(device)
+                    nb_patches_y, nb_patches_x = patch.shape[1:]
+                    embeddings = model[0].forward_features(patch)["x_norm_patchtokens"].reshape(nb_patches_y, nb_patches_x, feat_dim)
+                    central_embedding = embeddings[nb_patches_y//2, nb_patches_x//2]
+                    central_embedding = central_embedding.unsqueeze(0)
+                    output = model[1](central_embedding)
                     #UMAP:
-                    UMAP_LIST.extend(embeddings)
-                    UMAP_LABELS.extend(ground_truths)
+                    if use_umap and epoch == 0:    
+                        UMAP_LIST.extend(central_embedding.cpu().numpy())
+                        UMAP_PREDICTED_LABELS.extend(output.cpu().numpy())
+                    y_coord = n // H_patch
+                    x_coord = n % W_patch
+                    prediction[y_coord: y_coord + patch_size, x_coord: x_coord + patch_size] = torch.zeros(patch_size, patch_size) if round(output) == 0 else torch.ones(patch_size, patch_size)
                     
-                
-                embeddings = embeddings.to(device)
-                ground_truths = ground_truths.to(device)
-                
-
-                outputs = model(embeddings)
-
-                for output, gt in zip(outputs, ground_truths):
-                    predicted_idx = torch.round(output)
-                    true_idx = gt
-                    assert type(predicted_idx) == type(true_idx), "predicted_idx and true_idx must be of the same type"
-
-                    prediction_list.append([int(predicted_idx), int(true_idx)])
-
-                    if predicted_idx == true_idx:
-                        score += 1
-                    total += 1
-                    
-            batch_score = 100 * score / total if total > 0 else 0
-            test_accuracies.append(batch_score)
-
+                loss = loss_fn(ground_truth, prediction)
+            test_loss_list.append(loss)
 
     #UMAP
     if use_umap:
@@ -130,32 +133,30 @@ def training_block(model,
         UMAP_EMBEDDINGS = reducer.fit_transform(UMAP_LIST)
 
         plt.clf()
-        unique_labels = np.unique(UMAP_LABELS)
+        unique_labels = np.unique(UMAP_PREDICTED_LABELS)
         
         colors = plt.cm.bwr(np.linspace(0,1,len(unique_labels)))
         for i, label in enumerate(unique_labels):
-            mask = UMAP_LABELS == label
+            mask = UMAP_PREDICTED_LABELS == label
             if label == 0:
                 plot_label = 'Rest'
             else:
                 plot_label = 'PSD'
         plt.scatter(UMAP_EMBEDDINGS[mask, 0], UMAP_EMBEDDINGS[mask, 1], c=[colors[i]], s=0.5, label=plot_label, marker='o')
         plt.legend()
-        plt.title(f'UMAP - accuracy: {batch_score}')
+        plt.title(f'UMAP - accuracy: {loss}')
         plt.show()
         #plt.savefig(os.path.join('/home/tomwelch/Cambridge/Figures', f'UMAP_{batch_score:.2f}.png'))
 
 
     if return_statistics:
-        return loss_list, prediction_list, test_accuracies, model.state_dict()
+        return loss_list, prediction_list, test_loss_list, model.state_dict()
 
     else:
-        return loss_list[-1], test_accuracies[-1]
+        return loss_list[-1], test_loss_list[-1]
 
 
 def fine_tuning(model,
-                train_set,
-                test_set,
                 nb_iterations, 
                 nb_epochs_per_iteration):
     block_names = ['DINOv2', 'MLP Head']
@@ -165,10 +166,17 @@ def fine_tuning(model,
         
     general_loss_list, general_accuracy_list = [[],[]], [[], []] # first is dino, second is mlp head
 
+    train_set, test_set = get_dataset(nb_best_patches=1,
+                                      resize_size=resize_size, 
+                                      padding_size=5, 
+                                      test_proportion=0.2,
+                                      batch_size=1)
+
+
     i = 1
     for index in tqdm(index_list, desc='Iterations'):
 
-        loss, accuracy = training_block(model=model,
+        loss, test_loss = training_block(model=model,
                                         index=index, 
                                         nb_epochs=nb_epochs_per_iteration, 
                                         train_set=train_set, 
@@ -179,16 +187,14 @@ def fine_tuning(model,
         print(f'Block trained: {block_names[index]} | Iteration: {i}/{nb_iterations} | Train loss: {loss:.3f} | Accuracy: {accuracy:.2f}%')
         i += index
         general_loss_list[index].append(loss)
-        general_accuracy_list[index].append(accuracy)
+        general_accuracy_list[index].append(test_loss)
         
     return general_loss_list, general_accuracy_list 
 
 if __name__ == '__main__':
     general_loss_list, general_accuracy_list = fine_tuning(model=augmented_model,
-                train_set=train_loader,
-                test_set=test_loader,
-                nb_iterations=2, 
-                nb_epochs_per_iteration=40)
+                                                           nb_iterations=2, 
+                                                           nb_epochs_per_iteration=40)
 
     x = [i for i in range(len(general_loss_list[0]))]
     fig, ax1 = plt.subplots(figsize=(7, 5), dpi=150)
--- src/fine_tuning/psd_prediction/sliding_window_dataset.py
+++ src/fine_tuning/psd_prediction/sliding_window_dataset.py
@@ -2,76 +2,126 @@ from tqdm import tqdm
 import numpy as np
 from torch.utils.data import Dataset
 import torch.utils.data as utils
-from random import sample
 import torch
 import torchvision
 
-from src.setup import neurotransmitters
+#from src.setup import neurotransmitters, model
 from src.perso_utils import get_fnames, load_image
 from src.analysis_utils import resize_hdf_image
 
-def get_dataset(resize_size:int, window_size:int = 3):
+
+# TODO: ADD EMBEDDING COMPUTATION FUNCTION AS DEFAULT FIRST STEP
+
+import os
+import random 
+from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
+
+from src.setup import embeddings_path, model, feat_dim
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# LOADING DATA:
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+print('-Loading embeddings')
+
+_EMBEDDINGS = torch.load(os.path.join(embeddings_path, 'small_dataset_embs_518.pt'))
+EMBEDDINGS=[]
+for e in _EMBEDDINGS:
+    EMBEDDINGS.extend(e)
+EMBEDDINGS = EMBEDDINGS[:2]
+
+REFS = torch.load(os.path.join(embeddings_path, 'small_mean_ref_518_Aug=False_k=10.pt'), weights_only=False)
+
+print('...done loading embeddings')
+
+nb_best_patches = 1
+
+def get_dataset(nb_best_patches: int = 1,
+                resize_size:int = 518, 
+                padding_size:int = 1, 
+                test_proportion:float = 0.2,
+                batch_size:int = 1): # padding_size should be an odd number
     
-    patch_size = model.patch_size
-    
+    patch_size = model.patch_size # type: ignore 
+
     assert resize_size % patch_size == 0, f'crop size must be a multiple of patch_size = {patch_size}'
 
     files, _ = zip(*get_fnames())
 
-    IMAGES = []
-    for file in tqdm(files, desc='Loading images'):
+    DATASET = []
+
+    for k, file in tqdm(enumerate(files), desc='Loading patches'):
+        PATCHES = []
         image, _, _ = load_image(file)
         resized_image = resize_hdf_image(image, resize_size=resize_size).squeeze()
-        nb_x, nb_y = resized_image.shape // patch_size
+        x, y = resized_image.shape
         
-        for i in range(0, nb_x, patch_size):
-            start_x = i
-            end_x = i + window_size
-            for j in range(0, nb_y, patch_size):
-                start_y = j
-                end_y = j + window_size
+        H_patch, W_patch = resized_image.shape
+        flattened_image = resized_image.reshape(-1, feat_dim)
+
+        # Compute similarity
+        similarity_matrix = euclidean_distances(REFS, flattened_image)
+
+        # Get top K matches
+        flat_similarities = similarity_matrix.ravel()
+        top_flat_indices = flat_similarities.argsort()[:nb_best_patches]
+
+        # Map to (ref_idx, patch_idx)
+        ref_indices, patch_indices = np.unravel_index(top_flat_indices, similarity_matrix.shape)
+
+        y_coord = patch_indices // H_patch
+        x_coord = patch_indices % W_patch
+        
+        gt_y_start_idx = y_coord * patch_size
+        gt_y_end_idx = (y_coord + 1) * patch_size
+
+        gt_x_start_idx = x_coord * patch_size
+        gt_x_end_idx = (x_coord + 1) * patch_size
+        
+        GT = torch.zeros((y, x))
+        GT[gt_y_start_idx:gt_y_end_idx, gt_x_start_idx:gt_x_end_idx] = torch.ones((patch_size, patch_size))
+
+
+        padding = padding_size * patch_size
+        
+        for i in range(0, x, patch_size):
+            start_x = max(i - padding, 0)
+            end_x = min(i + padding + patch_size, x)
+            for j in range(0, y, patch_size):
+                start_y = max(j - padding, 0)
+                end_y = min(j + padding + patch_size, y)
                 
-                patch = resized_image[start_x:end_x, start_y:end_y]
-        
-        
-        
-        stack = np.concatenate([cropped_im, cropped_im, cropped_im], axis=2)
-        IMAGES.append(stack)
-    IMAGES = np.array(IMAGES).transpose(0,3,1,2)
+                patch = resized_image[start_x:end_x, start_y:end_y][...,None]
+                stack = np.concatenate([patch, patch, patch], axis=2)
+                stack = stack.transpose(2,0,1)[None]
+                PATCHES.append(stack)
+                
+        DATASET.append([PATCHES, GT])
 
-    FT_LABELS = np.hstack([[neuro]*600 for neuro in neurotransmitters]).reshape(-1,1)
+    random.shuffle(DATASET)
 
-    FT_DATASET = list(zip(IMAGES, FT_LABELS))
+    nb_images = len(DATASET)
 
-    FT_DATASET = sample(FT_DATASET, len(FT_DATASET))
+    SPLIT = int(nb_images*test_proportion)
+    TRAINING_SET = DATASET[SPLIT:]
+    TEST_SET = DATASET[:SPLIT]
 
-    test_proportion = 0.2
+    training_dataset = Custom_Dataset(TRAINING_SET) 
+    test_dataset = Custom_Dataset(TEST_SET)
 
-    FT_SPLIT = int(len(FT_DATASET)*test_proportion)
-    FT_TRAINING_SET = FT_DATASET[FT_SPLIT:]
-    FT_TEST_SET = FT_DATASET[:FT_SPLIT]
-    class Custom_FT_Dataset(Dataset):
-        def __init__(self, 
-                    set):
-            if set == 'training':
-                self.data = FT_TRAINING_SET
-            else:
-                self.data = FT_TEST_SET
+    training_loader = utils.DataLoader(training_dataset, batch_size=batch_size)
+    test_loader = utils.DataLoader(test_dataset, batch_size=batch_size)
 
-        def __len__(self):
-            return len(self.data)
+    return training_loader, test_loader
 
-        def __getitem__(self, idx):
-            image, label = self.data[idx]
-            label_idx = neurotransmitters.index(label[0])
-            return image, one_hot_neurotransmitters[label_idx]
-        
-    ft_train_batch_size, ft_test_batch_size = 1, 1
 
-    ft_training_dataset = Custom_FT_Dataset('training') 
-    ft_test_dataset = Custom_FT_Dataset('test')
-
-    ft_training_loader = utils.DataLoader(ft_training_dataset, batch_size=ft_train_batch_size, shuffle=True, pin_memory=True)
-    ft_test_loader = utils.DataLoader(ft_test_dataset, batch_size=ft_test_batch_size, shuffle=True, pin_memory=True)
-    
-    return ft_training_loader, ft_test_loader
+class Custom_Dataset(Dataset):
+    def __init__(self,
+                 SET):
+        self.data = SET
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self, idx):
+        return self.data[idx]
